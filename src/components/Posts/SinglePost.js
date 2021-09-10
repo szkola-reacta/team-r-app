b677b45cb5f20fd2812956bd82e03439f72d5247
patryk406 <49455624+patryk406@users.noreply.github.com>
2021-09-10 12:59:19 +0200

    Data fetching is fixed, as well - keys for maping.

--- src/components/Posts/SinglePost.js
+++ src/components/Posts/SinglePost.js
@@ -4,7 +4,7 @@ import { Parallax } from 'react-parallax';
 import './style.scss';
 const SinglePost = () => {
 
-    const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState();
 
     useEffect(() => {
         fetchPosts()
@@ -21,30 +21,30 @@ const SinglePost = () => {
     return (
         <div className='post_container'>
             <section className='post_header'>
-                <h3 className='post_title'>{posts.post_description.title}</h3>
-                <img src={posts.title_image} alt="art that is presenting title" />
+                <h3 className='post_title'>{posts && posts.post_description.title}</h3>
+                <img src={posts && posts.title_image} alt="art that is presenting title" />
             </section>
             <section className='post_body'>
                 <div className='post_content'>
-                    <h1>Today we're traveling to - {posts.post_description.location}</h1>
-                    <Parallax blur='3' bgImage={posts.post_description.description.img_link} bgImageAlt="the lake" strength={150}>
+                    <h1>Today we're traveling to - {posts && posts.post_description.location}</h1>
+                    <Parallax blur='3' bgImage={posts && posts.post_description.description.img_link} bgImageAlt="the lake" strength={150}>
                         <div className='post_paraph'>
-                            {posts.post_description.description.par1}
+                            {posts && posts.post_description.description.par1}
                         </div>
 
                         <div className='post_paraph'>
-                            {posts.post_description.description.par2}
+                            {posts && posts.post_description.description.par2}
                         </div>
                     </Parallax>
-                    <div className='post_places'><h4>Places near that location:</h4> <div>{posts.post_description.description.places_near.map(elem => <p>{elem}</p>)}</div></div>
+                    <div className='post_places'><h4>Places near that location:</h4> <div>{posts && posts.post_description.description.places_near.map(elem => <p key={`posts-${Math.random()}-paraph`}>{elem}</p>)}</div></div>
                 </div>
             </section>
             <section className='post_footer'>
                 <div className='post_author'>
-                    <h5> Author: {posts.author.job} {posts.author.name} Recoment these #Tags:</h5>
+                    <h5> Author: {posts && posts.author.job} {posts && posts.author.name} Recoment these #Tags:</h5>
                 </div>
                 <div className='post_tags'>
-                    {posts.tags.map((elem) => <label>{elem}</label>)}
+                    {posts && posts.tags.map((elem) => <label key={`tags-${Math.random()}-tag`}>{elem}</label>)}
                 </div>
             </section>
         </div>
